f717693e40a4077a001484502fff25f6106d98e0
lalitote <beata.wasylkiewicz@gmail.com>
2016-11-16 10:41:31 +0100

    Cassini Demo - beginning of lecture 8
    Developing iOS Apps with Swift / cs193P. Cassini Demo.

---
+++ Cassini/CassiniViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CassiniViewController.swift
+//  Cassini
+//
+//  Created by lalitote on 16.11.2016.
+//  Copyright © 2016 lalitote. All rights reserved.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController {
+
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+
+}
---
+++ Cassini/DemoURL.swift
@@ -0,0 +1,27 @@
+//
+//  URL.swift
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2016 Stanford University. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL
+{
+    static let Stanford = "http://66.media.tumblr.com/tumblr_lq5l6uO6sW1r1killo1_1280.jpg"
+
+    static let NASA = [
+        "Cassini" : "http://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg",
+        "Earth" : "http://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg",
+        "Saturn" : "http://www.nasa.gov/sites/default/files/saturn_collage.jpg"
+    ]
+    
+    static func NASAImageNamed(imageName: String?) -> NSURL? {
+        if let urlstring = NASA[imageName ?? ""] {
+            return NSURL(string: urlstring)
+        } else {
+            return nil
+        }
+    }
+}
