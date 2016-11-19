cc8180fcc2bfe76d354b4572aef9e3d6bda61911
lalitote <beata.wasylkiewicz@gmail.com>
2016-11-19 22:42:31 +0100

    buttons instead of seques in case of split view
    To reuse actual MVC

--- Cassini/CassiniViewController.swift
+++ Cassini/CassiniViewController.swift
@@ -24,6 +24,15 @@ class CassiniViewController: UIViewController, UISplitViewControllerDelegate {
         }
     }
 
+    @IBAction func showImage(_ sender: UIButton) {
+        if let ivc = splitViewController?.viewControllers.last?.contentViewController as? ImageViewController {
+            let imageName = sender.currentTitle
+            ivc.imageURL = DemoURL.NASAImageNamed(imageName: imageName)
+            ivc.title = imageName
+        } else {
+            performSegue(withIdentifier: Storyboard.ShowImageSegue, sender: sender)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         splitViewController?.delegate = self
